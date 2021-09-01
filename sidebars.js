1ee076837988b462e0ca3528d7c7af1e123c31ee
max <mfahampshire@gmail.com>
2021-09-01 23:05:55 +0200

    tidied up comments

--- sidebars.js
+++ sidebars.js
@@ -1,17 +1,5 @@
-/**
- * Creating a sidebar enables you to:
- - create an ordered group of docs
- - render a sidebar for each doc of that group
- - provide next/previous navigation
-
- The sidebars can be generated from the filesystem, or explicitly defined here.
-
- Create as many sidebars as you want.
- */
-
- module.exports = {
+module.exports = {
   sidebar: {
-    // intro.md needs to be included somewhere 
     "Overview": [
       "overview/index",
       "overview/nym-platform",
