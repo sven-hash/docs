68f83cc5c0382f993be10914e3e9e6a0c5923470
mx <maxhampshire@pm.me>
2022-12-13 13:57:49 +0100

    updated config for versioning

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -98,12 +98,12 @@ module.exports = {
           lastVersion: undefined,
           versions: {
             current: {
-              label: 'v1.1.3 (in progress)',
+              label: 'v1.1.4 (in progress)',
               path: 'next',
               banner: 'unreleased',
             },
             stable: {
-              label: 'v1.1.2 (stable)',
+              label: 'v1.1.3 (stable)',
               path: 'stable',
               banner: 'none',
             },
